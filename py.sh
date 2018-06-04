dacf70c21a760dceb5c0edd8d16bb0cdee29dfee
bot <36794992+jason6694@users.noreply.github.com>
2018-06-04 20:24:26 +0800

    Update py.sh

--- py.sh
+++ py.sh
@@ -1,17 +1,12 @@
 #!bin/bash
-000000=/home/booooob456/000000
+000000='/home/booooob456/000000'
 tmux new -s 000000
 cd $000000
 python3 LH.py
-^b
-d
 tmux new -s py3bot1
 cd /home/booooob456/py3bot1
 python3 bot.py
-^b
-d
 tmux new -s py3bot5
 cd /home/booooob456/py3bot5
 python3 bot.py
-^b
-d
+
