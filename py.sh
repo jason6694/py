222ecf19309d03424b6f8780d86a61efb63029a0
bot <36794992+jason6694@users.noreply.github.com>
2018-06-04 20:53:16 +0800

    Add files via upload

--- py.sh
+++ py.sh
@@ -1,10 +1,13 @@
 #!bin/bash
 tmux new -s 000000
-cd \home\booooob456\000000
+cd /home/booooob456/000000
 python3 LH.py
+tmux detach
 tmux new -s py3bot1
-cd \home\booooob456\py3bot1
+cd /home/booooob456/py3bot1
 python3 bot.py
+tmux detach
 tmux new -s py3bot5
-cd \home\booooob456\py3bot5
+cd /home/booooob456/py3bot5
 python3 bot.py
+tmux detach
